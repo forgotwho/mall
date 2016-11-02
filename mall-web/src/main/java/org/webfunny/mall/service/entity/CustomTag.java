834034a2dc47d4e680d880223b9ff7d411822a78
webfunny <webfunny@sina.com>
2016-11-02 20:16:34 +0800

    test

--- mall-web/src/main/java/org/webfunny/mall/service/entity/CustomTag.java
+++ mall-web/src/main/java/org/webfunny/mall/service/entity/CustomTag.java
@@ -1,22 +1,16 @@
 package org.webfunny.mall.service.entity;
 
 import javax.persistence.Entity;
-<<<<<<< HEAD
-=======
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
->>>>>>> 1dd2ba724031f690f9885f3de97ce6f249e07ce9
 
 @Entity
 public class CustomTag {
 
-<<<<<<< HEAD
-=======
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
->>>>>>> 1dd2ba724031f690f9885f3de97ce6f249e07ce9
 	private Long tagId;//标签Id
 	private Integer sortNum;//显示顺序
 
@@ -28,13 +22,10 @@ public class CustomTag {
 		this.tagId = tagId;
 		this.sortNum = sortNum;
 	}
-<<<<<<< HEAD
-=======
 	
 	public Long getId() {
 		return id;
 	}
->>>>>>> 1dd2ba724031f690f9885f3de97ce6f249e07ce9
 
 	public Long getTagId() {
 		return tagId;
