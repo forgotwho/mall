64daad4beae9253910f3e38f48bac0485876cf27
zhangxuesheng <zhangxuesheng@forte.com.cn>
2016-11-15 17:45:17 +0800

    banner

--- mall-antd/src/front/components/Banner.js
+++ mall-antd/src/front/components/Banner.js
@@ -8,19 +8,39 @@ const Banner = React.createClass({
   getInitialState() {
     return {
       data: [],
+      pcData:[],
+      mobileData:[],
       background:''
     };
   },
   fetch(params = {}) {
-    $.get('/api/banner',function(data){
-      this.setState({data: data,background:data[0].color});
+    var param = {recommend:true};
+    $.get('/api/banner',function(result){
+      var pcData = [];
+      var mobileData = [];
+      result.map(function(data) {
+        if(data.source=="01"){
+          pcData.push(data);
+        }else if(data.source=="02"){
+          mobileData.push(data);
+        }else{
+          pcData.push(data);
+          mobileData.push(data);
+        }
+      });
+      this.setState({data: result,pcData:pcData,mobileData:mobileData,background:pcData[0].color});
 		}.bind(this));
   },
   componentDidMount() {
     this.fetch();
   },
   onChange(index) {
-    this.setState({background:this.state.data[index].color});
+     index = index +1;
+     if(index>=this.state.pcData.length){
+       index = 0;
+     }
+     console.log(index,this.state.pcData[index].color);
+    this.setState({background:this.state.pcData[index].color});
   },
   render() {
     var pcPicture = [];
@@ -29,46 +49,32 @@ const Banner = React.createClass({
     var pcLink = "";
     var mobileDefatult = "";
     var mobileLink = "";
-    this.state.data.map(function(data) {
-      if(data.source=="01"){
-        if(pcDefatult==""){
-          pcDefatult = data.picture;
-          pcLink = data.link;
-        }else{
-          pcPicture.push(<div key={data.picture}><a href={data.link}><img  width="100%" src={"/api/img/thumb/"+data.picture} /></a></div>);
-        }
-      }else if(data.source=="02"){
-        if(mobileDefatult==""){
-          mobileDefatult = data.picture;
-          mobileLink = data.link;
-        }else{
-          mobilePicture.push(<div key={data.picture}><a href={data.link}><img width="100%" src={"/api/img/thumb/"+data.picture} /></a></div>);
-        }
+    this.state.pcData.map(function(data) {
+      if(pcDefatult==""){
+        pcDefatult = data.picture;
+        pcLink = data.link;
       }else{
-        if(pcDefatult==""){
-          pcDefatult = data.picture;
-          pcLink = data.link;
-        }else{
-          pcPicture.push(<div key={data.picture}><a href={data.link}><img width="100%" src={"/api/img/thumb/"+data.picture} /></a></div>);
-        }
-        if(mobileDefatult==""){
-          mobileDefatult = data.picture;
-          mobileLink = data.link;
-        }else{
-          mobilePicture.push(<div key={data.picture}><a href={data.link}><img width="100%" src={"/api/img/thumb/"+data.picture} /></a></div>);
-        }
+        pcPicture.push(<div key={data.picture} style={{width:990,height:300}}><a href={data.link}><img  width="990" height="300" src={"/api/img/thumb/"+data.picture} /></a></div>);
+      }
+    });
+    this.state.mobileData.map(function(data) {
+      if(mobileDefatult==""){
+        mobileDefatult = data.picture;
+        mobileLink = data.link;
+      }else{
+        mobilePicture.push(<div key={data.picture}><a href={data.link}><img width="100%" src={"/api/img/thumb/"+data.picture} /></a></div>);
       }
     });
   return (
-    <Row type="flex" justify="center" style={{background:this.state.background}}>
-      <Col xs={{span:0}} sm={{span:0}} md={{span:24}} lg={{span:24}} style={{width:990,height:300}}>
+    <Row type="flex" justify="center" style={{background:this.state.background,height:300}}>
+      <Col xs={{span:0}} sm={{span:0}} md={{span:24}} lg={{span:24}} >
         <Carousel autoplay beforeChange={this.onChange}>
-          <div><a href={pcLink}><img width="100%" src={"/api/img/thumb/"+pcDefatult} /></a></div>
+          <div style={{width:990,height:300}}><a href={pcLink}><img width="990" height="300" src={"/api/img/thumb/"+pcDefatult} /></a></div>
           {pcPicture}
         </Carousel>
       </Col>
       <Col xs={{span:24}} sm={{span:24}} md={{span:0}} lg={{span:0}} style={{width:990,background:'#FFFFFF'}}>
-        <Carousel autoplay beforeChange={this.onChange}>
+        <Carousel autoplay >
           <div><a href={mobileLink}><img width="100%" height="100%" src={"/api/img/thumb/"+mobileDefatult} /></a></div>
           {mobilePicture}
         </Carousel>
